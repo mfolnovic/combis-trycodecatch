1b0c67da98e27f0c969bdc5686f9b83fe32cf062
Matija Folnovic <mfolnovic@gmail.net>
2017-04-09 04:24:05 +0200

    design done

--- explorer-web-ui/src/components/RankUser.js
+++ explorer-web-ui/src/components/RankUser.js
@@ -6,14 +6,7 @@ import Divider from 'material-ui/Divider';
 function RankUser({users}) {
   let items = [];
   users.forEach((user, i) => {
-    items.push(<User key={user.id + 'a'} rank={i} user={user}/>);
-    items.push(<User key={user.id + 'b'} rank={i} user={user}/>);
-    items.push(<User key={user.id + 'c'} rank={i} user={user}/>);
-    items.push(<User key={user.id + 'd'} rank={i} user={user}/>);
-    items.push(<User key={user.id + 'e'} rank={i} user={user}/>);
-    items.push(<User key={user.id + 'f'} rank={i} user={user}/>);
-    items.push(<User key={user.id + 'g'} rank={i} user={user}/>);
-    items.push(<User key={user.id + 'h'} rank={i} user={user}/>);
+    items.push(<User key={user.id} rank={i} user={user}/>);
     if (i + 1 < users.length) {
       items.push(<Divider key={'div' + i} inset={false}/>);
     }
